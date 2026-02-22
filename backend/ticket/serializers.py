28ee526a446e6a109b7ae3c1b805440974940740
Prisma4 <theprismarindark@yandex.com>
2026-02-22 13:18:54 +0400

    Small serializers fix

--- backend/ticket/serializers.py
+++ backend/ticket/serializers.py
@@ -9,6 +9,7 @@ class TicketSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = (
             "user",
+            "status",
             "processed_by"
         )
 
