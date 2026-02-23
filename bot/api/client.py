d34778031cd9f455e90a360db416428e0803cde5
Prisma4 <theprismarindark@yandex.com>
2026-02-23 14:44:21 +0400

    Bugfix

--- bot/api/client.py
+++ bot/api/client.py
@@ -17,33 +17,33 @@ client = ApiClient(
 
 client.register_endpoint(
     name="get_ticket_messages",
-    path="/tickets/ticket_messages/list_messages_for_ticket",
+    path="tickets/ticket_messages/list_messages_for_ticket",
     query_validator=PaginatedRetrieveQuery,
     response_validator=PaginatedTicketMessages,
     method="GET"
 )
 client.register_endpoint(
     name="create_ticket_message",
-    path="/tickets/ticket_messages/",
+    path="tickets/ticket_messages/",
     body_validator=CreateTicketMessage,
     method="POST"
 )
 client.register_endpoint(
     name="get_tickets",
-    path="/tickets/tickets/",
+    path="tickets/tickets/",
     query_validator=PageQuery,
     response_validator=PaginatedTickets,
     method="GET"
 )
 client.register_endpoint(
     name="create_ticket",
-    path="/tickets/tickets/",
+    path="tickets/tickets/",
     body_validator=CreateTicket,
     method="POST"
 )
 client.register_endpoint(
     name="close_ticket",
-    path="/tickets/tickets/{id}/close_ticket/",
+    path="tickets/tickets/{id}/close_ticket/",
     query_validator=RetrieveQuery,
     method="POST"
 )
